7a07ea972afd01e88a0f54b3086a0918da41b8cd
Abyan1234 <70338733+Abyan1234@users.noreply.github.com>
2022-11-16 21:28:29 -0500

    Add files via upload

---
+++ Car.js
@@ -0,0 +1,27 @@
+AFRAME.registerComponent("move-car",{
+    schema:{
+        moveX:{type:"number",default:1}
+    },
+    tick:function(){
+        this.data.moveX=this.data.moveX+0.01
+        var pos=this.el.getAttribute("position")
+        pos.x=this.data.moveX
+        this.el.setAttribute("position", {x:pos.x,y:pos.y,z:pos.z})
+    }
+})
+
+AFRAME.registerComponent("counter",{
+
+    update:function(){
+        window.addEventListener("click", (e) => {
+            this.data.clickCounter=this.data.clickCounter+1;
+            if(this.data.clickCounter === 1){
+                const rotation= {x:0, y:20,z:0};
+                this.el.setAttribute("rotation", rotation);
+            } else if(this.data.clickCounter === 2){
+                const rotation={x:0,y:100,z:0}
+            };
+        })
+       
+    }
+})
